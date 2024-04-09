c78f9c64f64c74d822e3397b71b5a9124a318182
Sarad Shrestha <saradctha50@gmail.com>
2024-04-09 22:52:51 +0545

    Initial Push

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="inc-exp-container">
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus" class="money plus">+{{ getIncome }}</p>
+        </div>
+        <div>
+            <h4>Expense</h4>
+            <p id="money-minus" class="money minus">{{ getExpenses }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  getIncome:{
+    type: Number,
+    required: true
+  },
+  getExpenses:{
+    type: Number,
+    required: true
+  },
+});
+
+
+</script>
